d6a002e57542349c047c2a76175cfacd8a2a69b6
Juan Pablo Sarria <juanpsarria@gmail.com>
2024-07-25 12:54:15 -0300

    tarea iniciada y finalizada

---
+++ main.py
@@ -0,0 +1,33 @@
+def compra_divisas(monto_ars, cotizacion_usd):
+    # Monto que se podría comprar
+    max_dolares = monto_ars / cotizacion_usd
+    print(f'El monto que se podría comprar es {max_dolares:.2f}.')
+    
+    # Cantidad de billetes de cada nominación
+    billetes_100 = int(max_dolares // 100)
+    resto = max_dolares - (billetes_100 * 100)
+    
+    billetes_20 = int(resto // 20)
+    resto -= billetes_20 * 20
+    
+    billetes_10 = int(resto // 10)
+    resto -= billetes_10 * 10
+    
+    # Monto realmente comprado
+    dolares_comprados = (billetes_100 * 100) + (billetes_20 * 20) + (billetes_10 * 10)
+    print(f'El monto comprado es {dolares_comprados}. Recibe {billetes_100} billetes de 100, {billetes_20} billetes de 20 y {billetes_10} billetes de 10')
+    
+    # Dinero sin gastar
+    dinero_sin_gastar = (max_dolares - dolares_comprados) * cotizacion_usd
+    print(f'Su vuelto es ${dinero_sin_gastar:.2f}.')
+    return {
+        "max_dolares": max_dolares,
+        "dolares_comprados": dolares_comprados,
+        "billetes_100": billetes_100,
+        "billetes_20": billetes_20,
+        "billetes_10": billetes_10,
+        "dinero_sin_gastar": dinero_sin_gastar
+    }
+
+
+compra_divisas(2050000, 1030)
---
+++ task.md
@@ -0,0 +1,13 @@
+Están diseñando una app para facilitar la compra de divisas. Inicialmente la limitan a dólares y pesos ARG.
+
+Disponen de un monto de dinero y conocen la cotización. Típicamente las casas de cambio no devuelven monedas y sólo disponen de billetes de determinada denominación. En este caso de 100, 20 y 10 dólares. Vuestra compra deberá ajustarse a estas condiciones.
+
+Se trata:
+
+a) Determinar el máximo valor en dólares que podrían comprar si no hubiesen las limitaciones indicadas.
+
+b) Determinar cuantos dólares realmente puedo comprar
+
+c) Determinar cuantos billetes de cada denominación recibiré, empezando por el de 100 dólares.
+
+d) Determinar cuanto dinero me queda sin gastar.
